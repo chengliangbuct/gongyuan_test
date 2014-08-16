a4c87ffc0467274f946daa1e6313c04670363121
公元 <gongyuan.cl@alibaba-inc.com>
2014-08-16 13:51:40 +0800

    test

---
+++ src/test/java/com/alibaba/gongyuan/insertTest.java
@@ -0,0 +1,77 @@
+package com.alibaba.gongyuan;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alibaba.gongyuan.entity.User;
+import com.alibaba.gongyuan.runner.JUnit4ClassLog4jRunner;
+import com.alibaba.gongyuan.service.UserService;
+
+import org.apache.log4j.Logger;  
+
+@RunWith(JUnit4ClassLog4jRunner.class)
+@ContextConfiguration(locations = "/spring-config.xml")
+//@Transactional
+public class insertTest {
+    
+    private static Logger logger = Logger.getLogger(insertTest.class);
+    
+    @Autowired UserService userService ;
+    
+    
+    
+    @Test
+    public void countAll(){
+        logger.info("数据库中的记录条数:"  + userService.countAll());
+        //System.out.println("数据库中的记录条数:"  + userService.countAll());
+    }
+    
+    @Test
+    public void insert(){
+        User user = new User();
+        user.setUsername("苏若年");
+        user.setPassword("passtest");
+        user.setEmail("dennisit@163.com");
+        user.setSex("男");
+        user.setAge(23);
+        userService.insert(user);
+    }
+    
+    @Test
+    public void selectAll(){
+        List<User> list = userService.selectAll();
+        for(int i=0; i<list.size(); i++){
+            User user = list.get(i);
+            System.out.println("用户名:" + user.getUsername() + "\t密码:" + user.getPassword() + "\t邮箱：" + user.getEmail());
+        }
+    }
+    
+    @Test
+    public void update(){
+        User user = new User();
+        user.setUsername("苏若年");
+        user.setPassword("xxxxxxxx");
+        user.setEmail("xxxxxx@163xxx");
+        user.setSex("男");
+        user.setAge(23);
+        userService.update(user);
+    }
+    
+    @Test
+    public void delete(){
+        userService.delete("苏若年");
+    }
+    
+    @Test
+    public void findByName(){
+        User user = userService.findByUserName("苏若年");
+        System.out.println("用户名:" + user.getUsername() + "\t密码:" + user.getPassword() + "\t邮箱：" + user.getEmail());
+
+    }
+}
